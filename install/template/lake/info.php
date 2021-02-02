cb3f069935d6c1e5d9b69c1767e5cf195beb6d4b
deatil <2217957370@qq.com>
2021-02-02 13:52:15 +0800

    更新模版

--- install/template/lake/info.php
+++ install/template/lake/info.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'name'    => 'lake', // 模板名称
-    'remark'  => '适用于手机端和PC端', // 模板简单介绍
+    'remark'  => '适用于手机端和PC端，模版页面来源于网络', // 模板简单介绍
     'cover'   => 'cover.png', // 后台显示模板缩列图 相对于模板目录路径
     'version' => '1.0.1', // 模板版本
     'author'  => 'deatil', // 作者
